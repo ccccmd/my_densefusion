5d2612c39de8e9632a9e560837885c5db0487779
ccccmd <chenmengda2009@163.com>
2020-04-22 23:22:56 +0800

    finish

--- train.py
+++ train.py
@@ -1,1 +1,16 @@
 #-*-coding:utf-8-*-
+
+import argparse
+import os
+import random
+import time
+import numpy as np
+import torch
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from datasets.dataset import PoseDataset as PoseDataset
+from model.PoseNet import PoseNet
+from model.PoseRefineNet import PoseRefineNet
+from loss.loss import Loss
+from loss.loss_refiner import Loss_refine
